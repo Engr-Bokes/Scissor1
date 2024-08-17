7fb1e014b81169a6ee5720339099e92ba5ff70b4
Maduabuchi Chibuokem <cbokesman@gmail.com>
2024-08-17 21:11:17 +0100

    commit15.1

--- scissor-backend/src/app.ts
+++ scissor-backend/src/app.ts
@@ -23,7 +23,7 @@ configureApp(app);
 await connectRedis();
 
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'https://scissor-url-shortner.hostless.app',
     optionsSuccessStatus: 200,
 };
 
